ad3c74823a5c8c35290eac18dc5fe70e74bc61d6
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-07-08 20:25:37 +0200

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+import AdBanner from './components/AdBanner';
+
+function App() {
+  return (
+    <div className="App">
+      <AdBanner />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
